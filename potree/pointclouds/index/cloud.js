a1f33edc925d2257e0574ee3cacb2b84b6941a51
philipblaine <pbme222@uky.edu>
2020-04-26 16:11:09 -0500

    add deliverables

---
+++ potree/pointclouds/index/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 2258235,
+    "boundingBox": {
+        "lx": 5517223.3,
+        "ly": 3810490.05,
+        "lz": 936.03,
+        "ux": 5519223.1899999999,
+        "uy": 3812489.94,
+        "uz": 2935.92000000013
+    },
+    "tightBoundingBox": {
+        "lx": 5517223.3,
+        "ly": 3810490.05,
+        "lz": 936.03,
+        "ux": 5519223.17,
+        "uy": 3812489.94,
+        "uz": 1405.64
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 17.319555282592775,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
